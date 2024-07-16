27c9451e2351c8d9b8ab88cff0fdbda039d3982a
Jackcvest <jackc.vest@gmail.com>
2024-07-16 12:10:49 -0500

    turtle lesson plans

---
+++ classStuff/turtleFunctions.py
@@ -0,0 +1,38 @@
+import turtle 
+
+turt = turtle.Turtle() # you can name your turtle anything, just rememeber you need to use that name before giving any function to them.
+
+### Turtle Functions ###
+
+
+# have the turtle draw a square in any fashion
+def draw_square():
+
+
+# have the turtle draw a triangle in any fashion
+def draw_triangle():
+
+
+
+
+# there is a very easy way to do this, and a very hard way to do this. (hint: look at the documentation!)
+def draw_circle(): 
+
+    
+
+
+# draw a smiley face! (hint: the face uses a function from draw circle, but cuts an argument in half)
+def draw_smiley():
+
+
+
+
+# have the turtle draw out your name! Use turtle.penup() and turtle.pendown() to help tell the turtle when you want it to draw.
+def draw_name(): 
+
+
+
+
+
+
+turtle.done() # keeps the screen on so you can look at your amazing work
